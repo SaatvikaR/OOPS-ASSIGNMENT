7445d9ea351b9d1602d390b6f965d1d6a0780dd2
SaatvikaR <saatvika0507@gmail.com>
2025-09-28 10:05:08 +0530

    Assignment3

---
+++ ThreadExample.java
@@ -0,0 +1,37 @@
+
+// Basic Thread Creation using Thread class
+
+class MyThread extends Thread {
+    // Overriding run() method
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Child Thread: " + i);
+            try {
+                Thread.sleep(500); // pause for 0.5 second
+            } catch (InterruptedException e) {
+                System.out.println(e);
+            }
+        }
+    }
+}
+
+public class ThreadExample {
+    public static void main(String[] args) {
+        // Create a thread object
+        MyThread t1 = new MyThread();
+
+        // Start the thread
+        t1.start();
+
+        // Main thread task
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Main Thread: " + i);
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                System.out.println(e);
+            }
+        }
+    }
+}
+
